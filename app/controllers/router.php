f6ac571d815493144a6747b0186222c53ca0c146
Fred <fred@rattap.com>
2011-07-23 01:37:14 -0700

    starting to write controllers

--- app/controllers/router.php
+++ app/controllers/router.php
@@ -1,16 +1,18 @@
 <?php
 
-$request = $_SERVER['QUERY_STRING'];
-$parsed = explode('&', $request);
-$page = array_shift($parsed);
-$getVars = array();
-foreach ($parsed as $argument) {
-  list($variable, $value) = split('=', $argument);
-  $getVars[$variable] = $value;
-}
+$request = $_SERVER['PATH_INFO'];
+echo $request;
+list($blank, $page, $func) = explode("/", $request);
 
-//this is a test , and we will be removing it later
-error_log("The page your requested is ".$page);
-error_log('<br/>');
-$vars = print_r($getVars, TRUE);
-error_log("The following GET vars were passed to the page:<pre>".$vars."</pre>");
+switch ($page) {
+  case 'users':
+    require_once('app/controllers/users_controller.php');
+    users_call($func);
+    break;
+  case 'public':
+    require_once('app/controllers/public_controller.php');
+    public_call($func);
+    break;
+  default:
+    error_log("[CTRL] router.php: ". $page);
+}
