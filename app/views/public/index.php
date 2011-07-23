98a6e5c701227e604b83631753aab9941e15d498
Riley <riley@rattap.com>
2011-07-23 04:57:58 -0700

    compatitiblity with the new controladero

--- app/views/public/index.php
+++ app/views/public/index.php
@@ -3,7 +3,7 @@
         <h1>ratTap</h1>
       </div>
       <div data-role="content">  
-        <form action="/users/new" method="POST">
+        <form action="/users/create" method="POST">
           <input type="text" name="name" class="defaultText" title="Your Name..." />
           <input type="hidden" name="latitude" id="latitude" />
           <input type="hidden" name="longitude" id="longitude" />
