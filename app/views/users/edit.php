1cd722ff1d4f6d14c5f2a01525ff126f0cde84c5
Kingston <web@kingstontam.com>
2011-07-23 07:52:34 -0700

    FOO

--- app/views/users/edit.php
+++ app/views/users/edit.php
@@ -3,7 +3,7 @@
         <h1>Edit Phone Number</h1>
       </div>
       <div data-role="content">  
-        <form rel="external" id="update-phone" action="/users/update" method="POST">
+        <form data-ajax="false" id="update-phone" action="/users/update" method="POST">
           <input type="text" name="phone" class="defaultText" title="Phone Number" />
           <input type="hidden" name="group_id" value="<?php echo $group_id ?>" />
           <input type="submit" value="Add" id="rattapbutton" />
