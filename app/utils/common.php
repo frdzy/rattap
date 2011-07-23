1e57fccd833307bc5fee805384984cbc428efcd3
Kingston <web@kingstontam.com>
2011-07-23 04:20:45 -0700

    Adding group helpers

--- app/utils/common.php
+++ app/utils/common.php
@@ -3,7 +3,10 @@
 // Loads common utility functions
 
 function error_404() {
-    header("Status: 404 Not Found");
-    exit;
+  header("Status: 404 Not Found");
+  exit;
 }
 
+function get_pdo_connection() {
+  return new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+}
