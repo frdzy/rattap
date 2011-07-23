fb59d7ddd3f4c3bedb73ec1b8d33ef4d6b968880
Riley <riley@rattap.com>
2011-07-23 03:02:41 -0700

    wooo yeah

--- app/views/public/index.php
+++ app/views/public/index.php
@@ -1,27 +1,6 @@
-<html> 
-	<head> 
-	<title>Page Title</title> 
-	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0b1/jquery.mobile-1.0b1.min.css" />
-	<script type="text/javascript" src="http://code.jquery.com/jquery-1.6.1.min.js"></script>
-	<script type="text/javascript" src="http://code.jquery.com/mobile/1.0b1/jquery.mobile-1.0b1.min.js"></script>
-</head> 
-<body> 
-
-<div data-role="page">
-
-	<div data-role="header">
-		<h1>Page Title</h1>
-	</div><!-- /header -->
-
-	<div data-role="content">	
-		<p>Page content goes here.</p>		
-	</div><!-- /content -->
-
-	<div data-role="footer">
-		<h4>Page Footer</h4>
-	</div><!-- /footer -->
-</div><!-- /page -->
-
-</body>
-</html>
-
+    <form action="/users/new" method="POST">
+      <input type="text" name="name" class="defaultText" title="Your Name..." />
+      <input type="hidden" name="latitude" id="latitude" />
+      <input type="hidden" name="longitude" id="longitude" />
+      <input type="submit" value="ratTap!" />
+    </form>
