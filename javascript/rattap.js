05ba58e2d3d662342c6a07782f2197af0614bef0
Riley <riley@rattap.com>
2011-07-23 02:23:08 -0700

    Basic interface from public index complete

---
+++ javascript/rattap.js
@@ -0,0 +1,18 @@
+// hint text for input text
+$(document).ready(function()
+{
+  $(".defaultText").focus(function(srcc) {
+    if ($(this).val() == $(this)[0].title) {
+      $(this).removeClass("defaultTextActive");
+      $(this).val("");
+    }
+  });
+  $(".defaultText").blur(function() {
+    if ($(this).val() == "") {
+      $(this).addClass("defaultTextActive");
+      $(this).val($(this)[0].title);
+    }
+  });
+  $(".defaultText").blur();        
+});
+
