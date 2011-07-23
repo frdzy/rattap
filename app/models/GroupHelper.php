21d01e524761e46d69ee9dad0104e26e79d2a0ae
Kingston <web@kingstontam.com>
2011-07-23 05:01:30 -0700

    Nearby groups work

--- app/models/GroupHelper.php
+++ app/models/GroupHelper.php
@@ -5,18 +5,19 @@ class GroupHelper {
     $group_name = $user['username'] . "'s Group";
     $sql = "INSERT INTO groups(groupcreatorid, groupname, longtitude, latitude) values(?, ?, ?, ?)";
     $sth = $conn->prepare($sql);
-    $sth->execute(array($user['id'], $group_name, $long, $lat));
+    $sth->execute(array($user['userid'], $group_name, $long, $lat));
+    return $conn->lastInsertId();
   }
 
   public static function joinGroup($conn, $group_id, $user) {
     $sql = "INSERT INTO group_user_assoc(groupid, userid) values(?, ?)";
     $sth = $conn->prepare($sql);
-    $sth->execute(array($group_id, $user['id']));
+    $sth->execute(array($group_id, $user['userid']));
   }
 
   public static function getNearbyGroups($conn, $long, $lat) {
-    $variance = 0.001;
-    $sql = "SELECT * FROM groups WHERE longitude BETWEEN ? AND ? AND latitude BETWEEN ? AND ?";
+    $variance = 0.01;
+    $sql = "SELECT * FROM groups WHERE longtitude BETWEEN ? AND ? AND latitude BETWEEN ? AND ?";
     $sth = $conn->prepare($sql);
     $sth->execute(array($long - $variance, $long + $variance, $lat - $variance, $lat + $variance));
     return $sth->fetchAll();
